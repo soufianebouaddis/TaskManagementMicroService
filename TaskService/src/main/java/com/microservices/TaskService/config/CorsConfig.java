962eadcb5e1288bb65ae09ab8c4c1c2d72024362
mjustatool <fousenaibdisbdis@gmail.com>
2023-12-28 01:08:53 +0100

    fixing security bugs and corsorigin

--- TaskService/src/main/java/com/microservices/TaskService/config/CorsConfig.java
+++ TaskService/src/main/java/com/microservices/TaskService/config/CorsConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173").allowedMethods("*").allowedHeaders("*");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
     }
 }
